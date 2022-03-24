eb0ce1ec6fdddb82f4fe35c5e3f88814049d2b94
alexcano98 <alexcanocos@gmail.com>
2022-03-24 17:22:04 +0100

    commit local

--- lanza_lotes_slurm.sh
+++ lanza_lotes_slurm.sh
@@ -1,1 +1,4 @@
-sh scripts/launcher_slurm.sh runfiles/flat_fly/flatflt_topology_2vcs params_script/ 1
+sh scripts/launcher_slurm.sh runfiles/flat_fly/flatfly_topology_2vcs params_script/ 1
+sh scripts/parser_get_stats.sh results/flat_fly/flatfly_topology_2vcs_sim
+python3 scripts/parser_get_plots.py results/flat_fly/flatfly_topology_2vcs_sim/parsed/ flatfly
+
