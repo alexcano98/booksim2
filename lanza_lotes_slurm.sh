b19c234a31e535dc863387b5b9a613c9d4648fd3
alexcano98 <alexcanocos@gmail.com>
2022-03-24 17:31:19 +0100

    cambios

--- lanza_lotes_slurm.sh
+++ lanza_lotes_slurm.sh
@@ -1,4 +1,2 @@
 sh scripts/launcher_slurm.sh runfiles/flat_fly/flatfly_topology_2vcs params_script/ 1
 sh scripts/parser_get_stats.sh results/flat_fly/flatfly_topology_2vcs_sim
-python3 scripts/parser_get_plots.py results/flat_fly/flatfly_topology_2vcs_sim/parsed/ flatfly
-
